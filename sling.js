115391c60fd984c7592c5d1738d276089b681b18
rithiekvaishnev <74966777+rithiekvaishnev@users.noreply.github.com>
2021-03-19 14:57:25 +0530

    Add files via upload

---
+++ sling.js
@@ -0,0 +1,33 @@
+class Sling{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.02,
+            length: 10
+        }
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+
+    }
+
+    attach(body){
+     this.bodyA = body;  
+    }
+
+    fly(){
+    this.sling.bodyA = null;
+     }
+
+    display(){
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.sling.pointB;
+            strokeWeight(4);
+             line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+    
+  
+
+}
